b92ca03ef97c985056855cf969d7f40d8c1c7094
cauecarvalho255 <cauecarvalho255@gmail.com>
2026-03-13 19:04:57 -0400

    Create Exercicio1.c

---
+++ LISTA_1/Exercicio1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    
+    int vetorA[6] = {1, 0, 5, -2, -5, 7};
+    int i;
+    int soma; 
+    
+    soma = vetorA[0] + vetorA[1] + vetorA[5];
+    printf("A soma eh: %i\n", soma);
+    
+    vetorA[4] =  100;
+    
+    for (i = 0; i < 6; i++){
+        printf("%i\n", vetorA[i]);
+    }
+    return 0;
+}
