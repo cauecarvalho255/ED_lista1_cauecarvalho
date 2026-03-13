1d443f020af49167ca6c7764e203e43a4782f832
cauecarvalho255 <cauecarvalho255@gmail.com>
2026-03-13 19:24:34 -0400

    create exercicio2.c

--- LISTA_1/exercicio2.c
+++ LISTA_1/exercicio2.c
@@ -1,1 +1,18 @@
-printf
+#include <stdio.h>
+
+int main(){
+    
+    int vetor [6];
+    int i;
+    
+    for(i = 0; i < 6; i++){
+       printf("Digite os valores do vetor:  ", i);
+       scanf("%i", &vetor[i]);
+    } 
+    
+    for(i = 0; i < 6; i++){
+        printf("[%i]\n", vetor[i]);
+    }
+    
+    return 0;
+}
